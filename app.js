eabe06b685773028bad3f7fc4d6e748e40c5ee37
Johnny Chang <johnny.chang@corp.nextlink.com>
2024-03-20 11:44:50 +0800

    Add handlebars package

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const app = express()
 const port = 3000
+
+app.engine('handlebars', handlebars.engine())
+app.set('view engine', 'handlebars')
 
 app.get('/', (req, res) => {
   res.send('Hello world')
